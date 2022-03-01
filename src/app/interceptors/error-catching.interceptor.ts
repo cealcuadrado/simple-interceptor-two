1c6eb5a9f560fba5f324cf863a2d1dc93eed92a9
CeAlCuadrado <cealcuadrado@outlook.com>
2022-03-01 11:35:38 -0300

    Impresión en consola de pasos de solicitudes por el interceptor

--- src/app/interceptors/error-catching.interceptor.ts
+++ src/app/interceptors/error-catching.interceptor.ts
@@ -7,6 +7,7 @@ import {
   HttpErrorResponse
 } from '@angular/common/http';
 import { catchError, Observable, throwError, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorCatchingInterceptor implements HttpInterceptor {
@@ -16,15 +17,22 @@ export class ErrorCatchingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const updatedRequest = this.updateRequest(request);
 
+    console.log('Passed through the interceptor in request');
+
     return next.handle(updatedRequest).pipe(
+      map(res => {
+        console.log('Passed through the interceptor in response');
+        return res;
+      }),
       catchError((error: HttpErrorResponse) => {
+        console.log(error);
         let errorMsg = '';
         if (error.error instanceof ErrorEvent) {
           console.log('Client side error');
-          errorMsg = `Error: ${error.error.message}`;
+          errorMsg = `Error: ${error.message}`;
         } else {
           console.log('Server side error');
-          errorMsg = `Error code: ${error.status}, Message:${error.error.message}`;
+          errorMsg = `Error code: ${error.status}, Message:${error.message}`;
         }
         console.log(errorMsg);
         return throwError(() => new Error(errorMsg));
